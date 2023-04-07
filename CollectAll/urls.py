79e6c0a3492bd4f8edcfea7ad3283cf3031a0586
jetty-setter <123393626+jetty-setter@users.noreply.github.com>
2023-04-07 11:33:14 -0500

    Delete CollectAll directory

--- CollectAll/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.index, name='index'),
-	path('my_collections/<int:pk>', views.MyCollectionsView.as_view(), name='my_collections'),
-]
-
-
